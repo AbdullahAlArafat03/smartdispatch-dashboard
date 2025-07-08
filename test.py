e28a54073bd059af886b4c283ef43f3d27f1a6df
AbdullahAlArafat03 <s202156290@kfupm.edu.sa>
2025-07-08 13:07:56 +0300

    Update test.py

--- test.py
+++ test.py
@@ -3,6 +3,6 @@ import streamlit as st
 
 with st.sidebar.expander("Settings"):
 
-st.selectbox("Basic", ["Basic 1", "Basic 2", "Basic 3"])
-
-st.selectbox("Advanced", ["Advanced 1", "Advanced 2", "Advanced 3"])
+  st.selectbox("Basic", ["Basic 1", "Basic 2", "Basic 3"])
+  
+  st.selectbox("Advanced", ["Advanced 1", "Advanced 2", "Advanced 3"])
