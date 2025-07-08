9afe81fe57db140b0aa4d1d5ee9e538e58faf373
AbdullahAlArafat03 <s202156290@kfupm.edu.sa>
2025-07-08 13:37:19 +0300

    Update test.py

--- test.py
+++ test.py
@@ -3,9 +3,8 @@ import streamlit as st
 
 with st.sidebar.expander("Settings"):
 
-  mode == st.radio(""Choose Settings Mode", ["Basic", "Advanced"])
-
-  if mode == "Basic":
-    st.selectbox("Basic", ["Basic 1", "Basic 2", "Basic 3"])
-  elif:
-    st.selectbox("Advanced", ["Advanced 1", "Advanced 2", "Advanced 3"])
+   mode = st.selectbox("Optimization Algorithm", ["ACO", "Genetic Algorithm", "Simulated Annealing"]) 
+   if mode == "ACO":
+        input = st.number_input("CO₂ Emission Limit", min_value=100,max_value=1000,value=10)
+        if input < 100:
+           st.write("Limit is below the minimum, try again.")
