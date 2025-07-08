9911d8c12b06634ec34200541ebf7fa3bc6bdbec
AbdullahAlArafat03 <s202156290@kfupm.edu.sa>
2025-07-08 13:12:35 +0300

    Update test.py

--- test.py
+++ test.py
@@ -3,6 +3,9 @@ import streamlit as st
 
 with st.sidebar.expander("Settings"):
 
-  st.selectbox("Basic", ["Basic 1", "Basic 2", "Basic 3"])
-  
-  st.selectbox("Advanced", ["Advanced 1", "Advanced 2", "Advanced 3"])
+  mode == st.radio(""Choose Settings Mode", ["Basic", "Advanced"])
+
+  if mode == "Basic":
+    st.selectbox("Basic", ["Basic 1", "Basic 2", "Basic 3"])
+  elif mode == "Advanced":
+    st.selectbox("Advanced", ["Advanced 1", "Advanced 2", "Advanced 3"])
